e007f78ac047432f76357b52315395fd6eba8336
Your Name <ustinovoleksij@gmail.com>
2022-05-17 09:50:13 +0300

    Moved articles to the React

---
+++ src/components/Application.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from "./Header";
+
+import Footer from "./Footer";
+
+const Application = () => {
+  return(
+      <main>
+        <Header />
+        <Footer />
+      </main>
+  )
+};
+
+export default Application;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+
+const Header = ()=> {
+    return (
+        <header className="header">
+<nav>
+    <ul>
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">About</a>
+        </li>
+        <li>
+            <a href="#">Contact us</a>
+        </li>
+    </ul>
+</nav>
+        </header>
+    )
+};
+
+export default Header;
